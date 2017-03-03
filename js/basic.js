d935b09df4c518ba75df36064ec12c2aa5220835
Diego Valbuena Pineda <Diego Valbuena Pineda>
2017-03-03 22:12:51 +0100

    Added Basic Project, before create develop branch, first fi some basic features

---
+++ js/basic.js
@@ -0,0 +1,18 @@
+function basicLoad(){
+  document.getElementById("webTitle").innerHTML=webTitle;
+  document.getElementById("webHeader").innerHTML=webHeader;
+  document.getElementById("arduinoRasPiHeader").innerHTML=arduinoRasPiHeader;
+  document.getElementById("navHome").innerHTML=navHome;
+  document.getElementById("navTeam").innerHTML=navTeam;
+  document.getElementById("navHardware").innerHTML=navHardware;
+  document.getElementById("navLog").innerHTML=navLog;
+  document.getElementById("navLogIn").innerHTML=navLogIn;
+  document.getElementById("footerLink").innerHTML=footerLink;
+  document.getElementById("footerSentence").innerHTML=footerSentence;
+}
+
+
+/*DIRECT METHODS*/
+
+
+/* INSIDE METHODS*/
---
+++ js/hardware.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  basicLoad();
+  this.init(/*param1,param2*/);
+};
+
+/*ON DOCUMENT LOAD*/
+function init(/*param1,param2*/){
+
+}
+
+
+/*DIRECT METHODS*/
+
+
+/* INSIDE METHODS*/
