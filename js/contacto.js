d3c5db6d2108e83648178a97bd2451d3865dbd95
Diego Valbuena Pineda <Diego Valbuena Pineda>
2017-03-07 23:18:21 +0100

    set basic form imputs

--- js/contacto.js
+++ js/contacto.js
@@ -5,8 +5,16 @@ window.onload = function() {
 
 /*ON DOCUMENT LOAD*/
 function init(/*param1,param2*/){
-
+  document.getElementById("nickLabel").innerHTML=NickText;
+  document.getElementById("emailLabel").innerHTML=EmailText;
+  document.getElementById("evaluation").innerHTML=evaluation;
+  document.getElementById("suggest").innerHTML=suggest;
+  document.getElementById("review").innerHTML=review;
+  document.getElementById("accordance").innerHTML=accordance;
+  document.getElementById("textArea").innerHTML=texAreaContact;
 }
+
+var isAccordance = false;
 
 
 /*DIRECT METHODS*/
