d71ff255e567874f42ffc554505db08931d15e9a
javiesky <jaoliva@ucm.es>
2017-04-05 14:43:43 +0200

    first
    primera actualizacion

--- js/basic.js
+++ js/basic.js
@@ -1,5 +1,6 @@
 function basicLoad(){
-  document.getElementById("webTitle").innerHTML=webTitle;
+/* 
+ document.getElementById("webTitle").innerHTML=webTitle;
   document.getElementById("webHeader").innerHTML=webHeader;
   document.getElementById("arduinoRasPiHeader").innerHTML=arduinoRasPiHeader;
   document.getElementById("navHome").innerHTML=navHome;
@@ -10,7 +11,8 @@ function basicLoad(){
   document.getElementById("navContact").innerHTML=navContact;
   document.getElementById("footerLink").innerHTML=footerLink;
   document.getElementById("footerSentence").innerHTML=footerSentence;
-}
+*/
+  }
 
 
 /*DIRECT METHODS*/
