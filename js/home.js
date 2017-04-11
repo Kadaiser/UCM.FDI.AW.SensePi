f9a3766706d4c2526dd9572b07a7845df9964e23
Diego Valbuena Pineda <Diego Valbuena Pineda>
2017-04-11 11:32:43 +0200

    added canvas.js, minor fixed on login.html

--- js/home.js
+++ js/home.js
@@ -1,45 +1,10 @@
 window.onload = function() {
   this.init(/*param1,param2*/);
+  imageCanvasBorderArea();
 };
 
 /*ON DOCUMENT LOAD*/
 function init(/*param1,param2*/){
-
-  // get the target image
-  var img = byId('PBMap');
-
-  var x,y, w,h;
-
-  // get it's position and width+height
-  x = img.offsetLeft;
-  y = img.offsetTop;
-  w = img.clientWidth;
-  h = img.clientHeight;
-
-  // move the canvas, so it's contained by the same parent as the image
-  var imgParent = img.parentNode;
-  var can = byId('myCanvas');
-  imgParent.appendChild(can);
-
-  // place the canvas in front of the image
-  can.style.zIndex = 1;
-
-  // position it over the image
-  can.style.left = x+'px';
-  can.style.top = y+'px';
-
-  // make same size as the image
-  can.setAttribute('width', w+'px');
-  can.setAttribute('height', h+'px');
-
-  // get it's context
-  hdc = can.getContext('2d');
-
-  // set the 'default' values for the colour/width of fill/stroke operations
-  hdc.fillStyle = '#ff7a05';
-  hdc.strokeStyle = '#ff7a05';
-  hdc.lineWidth = 8;
-
   //Chart initialization
   google.charts.load('current', {'packages':['corechart']});
   google.charts.setOnLoadCallback(drawChart);
@@ -58,16 +23,11 @@ function init(/*param1,param2*/){
 /*DIRECT METHODS*/
 
 
-
 var mapView = true;
 
 function switchBetweenView(){
-  if(mapView){
-    switchToGalleryView();
-  }
-  else {
-    switchToMapView();
-  }
+  if(mapView){switchToGalleryView();}
+  else {switchToMapView();}
   mapView = !mapView;
 }
 
@@ -167,68 +127,7 @@ function drawChart() {
 
 /* INSIDE METHODS*/
 
-// stores the device context of the canvas we use to draw the outlines
-// initialized in myInit, used in myHover and myLeave
-var hdc;
 
-// shorthand func
-function byId(e){return document.getElementById(e);}
-
-// takes a string that contains coords eg - "227,307,261,309, 339,354, 328,371, 240,331"
-// draws a line from each co-ord pair to the next - assumes starting point needs to be repeated as ending point.
-function drawPoly(coOrdStr)
-{
-    var mCoords = coOrdStr.split(',');
-    var i, n;
-    n = mCoords.length;
-
-    hdc.beginPath();
-    hdc.moveTo(mCoords[0], mCoords[1]);
-    for (i=2; i<n; i+=2)
-    {
-        hdc.lineTo(mCoords[i], mCoords[i+1]);
-    }
-    hdc.lineTo(mCoords[0], mCoords[1]);
-    hdc.stroke();
-}
-
-function drawRect(coOrdStr)
-{
-    var mCoords = coOrdStr.split(',');
-    hdc.lineWidth = 0;
-    var top, left, bot, right;
-    left = mCoords[0];
-    top = mCoords[1];
-    right = mCoords[2];
-    bot = mCoords[3];
-    for (var i = 0; i < 8; i++) {
-      hdc.lineWidth = i;
-      setTimeout(hdc.strokeRect(left,top,right-left,bot-top), 1000);
-    }
-}
-
-function myHover(element)
-{
-    var hoveredElement = element;
-    var coordStr = element.getAttribute('coords');
-    var areaType = element.getAttribute('shape');
-    switch (areaType)
-    {
-        case 'polygon':
-        case 'poly':
-            drawPoly(coordStr);
-            break;
-
-        case 'rect':
-            drawRect(coordStr);
-    }
-}
-
-function myLeave()
-{
-    var canvas = byId('myCanvas');
-    hdc.clearRect(0, 0, canvas.width, canvas.height);
-}
 
 function mySpanAppear()
 {
