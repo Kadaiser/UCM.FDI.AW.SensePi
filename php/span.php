4b0ec09d31ebf7e8a2940eb3dd5b788ea24e2c24
FASUS\Kadaiser <dvalbuen@ucm.es>
2017-04-24 19:25:52 +0200

    General SidebarFloor user for index.php

---
+++ php/span.php
@@ -0,0 +1,15 @@
+<div class="span" id="Span">
+
+  <img class="staticImg" src="../images/icons/xOff.png">
+  <img class="hoverImg" src="../images/icons/xOn.png" alt="Close" onclick='mySpanHide();'>
+  <h1>CAFETERIA</h1>
+  <p>Noise:           40dB</p>
+  <div id="chart_div"></div>
+
+  <div class="favoriteMark">
+    <label>Add to favorite </label>
+    <img class="staticImg" src="../images/icons/slimStar.png" alt="Add to favorite">
+    <img class="hoverImg" src="../images/icons/fatStar.png">
+  </div>
+
+</div>
