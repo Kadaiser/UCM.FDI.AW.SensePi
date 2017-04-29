2efd960877b69929f0ddee1a1802f8ea0f1935f9
Diego Valbuena Pineda <Diego Valbuena Pineda>
2017-04-29 15:57:25 +0200

    Fixed xml reference

--- js/team.js
+++ js/team.js
@@ -9,110 +9,27 @@ function init(/*param1,param2*/){
 }
 
 /*DIRECT METHODS*/
-function switchMemberClick(selectedMember) {
-    nombre="desc";
-      num=0;
-      document.getElementById('desc').style.opacity=0;
-      document.getElementById('Diego').style.opacity=0;
-      document.getElementById('Javier').style.opacity=0;
-      document.getElementById('Julio').style.opacity=0;
-      document.getElementById('Sergio').style.opacity=0;
-  switch(selectedMember){
-    case 'Azahara':
-      document.getElementById("desc").style.display="block";     
-      document.getElementById("Diego").style.display="none";
-      document.getElementById("Javier").style.display="none";
-      document.getElementById("Julio").style.display="none";
-      document.getElementById("Sergio").style.display="none";
-      a=setInterval(open,30);
-      break;
-    case 'Diego':
-      document.getElementById("Azahara").style.display="none";     
-      document.getElementById("Diego").style.display="block";
-      document.getElementById("Javier").style.display="none";
-      document.getElementById("Julio").style.display="none";
-      document.getElementById("Sergio").style.display="none";
-      a=setInterval(open,30);
-      break;
-    case 'Javier':
-      document.getElementById("Azahara").style.display="none";     
-      document.getElementById("Diego").style.display="none";
-      document.getElementById("Javier").style.display="block";
-      document.getElementById("Julio").style.display="none";
-      document.getElementById("Sergio").style.display="none";
-          a=setInterval(open,30);
-      break;
-    case 'Julio':
-      document.getElementById("Azahara").style.display="none";     
-      document.getElementById("Diego").style.display="none";
-      document.getElementById("Javier").style.display="none";
-      document.getElementById("Julio").style.display="block";
-      document.getElementById("Sergio").style.display="none";
-      a=setInterval(open,30);
-      break;
-    case 'Sergio':
-      document.getElementById("Azahara").style.display="none";     
-      document.getElementById("Diego").style.display="none";
-      document.getElementById("Javier").style.display="none";
-      document.getElementById("Julio").style.display="none";
-      document.getElementById("Sergio").style.display="block";
-      a=setInterval(open,30);
-      break;
-    default:
-      document.getElementById("Azahara").style.display="none";     
-      document.getElementById("Diego").style.display="none";
-      document.getElementById("Javier").style.display="none";
-      document.getElementById("Julio").style.display="none";
-      document.getElementById("Sergio").style.display="none";
-  }
-}
-function open(a){
-    document.getElementById(nombre).style.opacity=num;
-    num=num+0.03;
-    if(num>=1)
-        clear();
-    
-}
-function clear(){
-    clearInterval(a);
-}
+
 /* INSIDE METHODS*/
 
 function loadText(a){
-  var xmlhttp;
-   if (window.XMLHttpRequest){
-     // Objeto para IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp=new XMLHttpRequest();
-  }else{
-    // Objeto para IE6, IE5
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-   
-  // Abrimos el archivo que esta alojado en el servidor cd_catalog.xml
-  xmlhttp.open("GET","../xml/team.xml",true);
-  xmlhttp.send();
 
-  // Obtenemos un objeto XMLDocument con el contenido del archivo xml del servidor
-  xmlDoc=xmlhttp.responseXML;
- 
-  // Obtenemos todos los nodos denominados foro del archivo xml
-  var names=xmlDoc.getElementsByTagName("name");
-  var surnames=xmlDoc.getElementsByTagName("surname");
-
-  var description=xmlDoc.getElementsByTagName("description");
-  var i = 0;
-  var ok = false;
-  var text;
-
-  while(i < names.length && !ok) {
-    if(names[i] == a){
-      text = "<h1>" + name[i] + surname[i] +"</h1>"+
-                  "<p>" + description[i] +"</p>";
-      ok = true;
+  var xhttp =new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      showInfo(this,a);
     }
-    document.getElementById("desc").innerHTML = text;
-    i++;
-  }
+  };
+  //requerir como metodo asincrono a la carga de la pagina (true)
+  xhttp.open("GET","../xml/team.xml",true);
+  xhttp.send();
+}
 
-  switchMemberClick(a);
+function showInfo(xml,a){
+  var info = "";
+  var xmlDoc = xml.responseXML;
+  var x = xmlDoc.getElementsByTagName("name");
+  info += "<h1>" + x[a].childNodes[0].nodeValue + "</h1>";
+  info += "<p>" + x[a].childNodes[0].nodeValue + "</p>";
+  document.getElementById("info").innerHTML = info;
 }
--- views/team.php
+++ views/team.php
@@ -21,187 +21,42 @@
       include '../php/navbar.php';
        ?>
 
-
 		<!-- CONTENT CLASS -->
 		<div id="content">
 
 			<div id="barraImagenes">
 				<ul>
           <li>
-            <div class="photo" onclick="loadText('Sergio')">
+            <div class="photo" onclick="loadText('2')">
 								<img id="sergio">
 	          </div>
-	            		  	<!-- <span>
-	            		  		Azahara
-	            		  	</span> -->
 					</li>
-
 					<li>
-            <div class="photo" onclick="loadText('Diego')">
+            <div class="photo" onclick="loadText('1')">
 								<img id="diego">
 	          </div>
-	            		  	<!-- <span>
-	            		  		Azahara
-	            		  	</span> -->
 					</li>
-
           <li>
-            <div class="photo" onclick="loadText('Azahara')">
+            <div class="photo" onclick="loadText('0')">
 								<img id="azahara">
 	          </div>
-	            		  	<!-- <span>
-	            		  		Azahara
-	            		  	</span> -->
 					</li>
-
           <li>
-            <div class="photo" onclick="loadText('Julio')">
+            <div class="photo" onclick="loadText('3')">
 								<img id="julio">
 	          </div>
-	            		  	<!-- <span>
-	            		  		Azahara
-	            		  	</span> -->
 					</li>
           <li>
             <div class="photo" onclick="loadText('Javier')">
 								<img id="javier">
 	          </div>
-	            		  	<!-- <span>
-	            		  		Azahara
-	            		  	</span> -->
 					</li>
 				</ul>
 			</div>
 
-<!--
-			<div id="desc" class="descripcionPersonal" style="display: none">
-				<h1>Azahara Fernández Notario</h1>
-				<p>
-					Soy Azahara una estudiante del Grado de Ingenieria Informática de la Universidad Complutense de Madrid en la rama de Tecnologías de la Información. Aunque actualmente resido en dicha ciudad nací en Talavera de la Reina (Toledo) un 21 de Julio de 1995.
-				</p>
-				<p>
-					Hija de Nuria y Josué, tengo tres hermanos pequeños, Yaiza, Josué y Alfredo. Me gustan el futbol, el airsoft, el rugby y los deportes en general. Además de practicar estos deportes en mi tiempo libre también me gusta leer, escuchar música o ver series.
-				</p>
-				<p>
-					Actualmente me dedico por completo a estudiar aunque tempranamente me gustaría incorporarme al mundo laboral, en el ámbito de las aplicaciones web o la programación en general.
-				</p>
 
-				<ul class="contactIcons">
-					<li>
-						<a href="https://github.com/azaferna" target="_blank">
-							<img src="../images/icons/git_icon.png" alt="GitHub">
-						</a>
-					</li>
-					<li>
-						<a href="https://www.facebook.com/nuria.notario.3" target="_blank">
-						  <img src="../images/icons/fb_icon.png" alt="Facebook">
-						</a>
-					</li>
-				</ul>
-			</div>
-
-			<div id="Diego" class="descripcionPersonal" style="display: none">
-
-				<ul class="contactIcons">
-					<li>
-						<a href="https://github.com/Kadaiser" target="_blank">
-							<img src="../images/icons/git_icon.png" alt="GitHub">
-						</a>
-					</li>
-					<li>
-						<a href="https://www.facebook.com/diego.valbuena.988" target="_blank">
-							<img src="../images/icons/fb_icon.png" alt="Facebook">
-						</a>
-					</li>
-				</ul>
-			</div>
-
-			<div id="Javier" class="descripcionPersonal" style="display: none">
-				<h1>Javier de la Oliva Sanchez - Valladares </h1>
-				<p>
-					Soy Javier, un estudiante del Grado de Ingenieria Informática de la Universidad Complutense de Madrid en la rama de Tecnologías de la Información. Aunque actualmente resido en dicha ciudad nací en Toledo en 1991.
-				</p>
-				<p>
-					Me gusta la música, aparte de escucharla también soy componente de varios grupos musicales. El deporte me gusta más verlo que practicarlo, entre ellos están el futbol, el tenis, y el mundo del motor en general.
-				</p>
-				<p>
-					Poseo el título de administración de sistemas informáticos en red (ASIR), y actualmente solo me dedico a estudiar el grado de ingeniería informática.
-				</p>
-
-				<ul class="contactIcons">
-					<li>
-						<a href="https://github.com/javiesky" target="_blank">
-							<img src="../images/icons/git_icon.png" alt="GitHub">
-						</a>
-					</li>
-					<li>
-						<a href="https://www.facebook.com/javi.esky" target="_blank">
-							<img src="../images/icons/fb_icon.png" alt="Facebook">
-						</a>
-					</li>
-				</ul>
-			</div>
-
-			<div id="Julio" class="descripcionPersonal" style="display: none">
-				<h1>Enrique Julio de la Cruz Valderrama </h1>
-				<p>
-					Soy Julio, un estudiante del Grado de Ingenieria Informática de la Universidad Complutense de Madrid en la rama de Tecnologías de la Información. Aunque actualmente resido en dicha ciudad nací en Lima.
-				</p>
-				<p>
-					Me encuentro cursando la carrera anteriormente mencionada, con el objetivo de complementar ciertos aspectos de mi entorno profesional.
-				</p>
-				<p>
-					Actualmente me dedico a la consultoría de sistemas informáticos en un ámbito público.
-				</p>
-
-				<ul class="contactIcons">
-					<li>
-						<a href="https://github.com/endela01" target="_blank">
-							<img src="../images/icons/git_icon.png" alt="GitHub">
-						</a>
-					</li>
-					<li>
-						<a href="https://www.facebook.com/" target="_blank">
-							<img src="../images/icons/fb_icon.png" alt="Facebook">
-						</a>
-					</li>
-				</ul>
-			</div>
-
-			<div id="Sergio" class="descripcionPersonal" style="display: none">
-				<h1>Sergio Calero Robles</h1>
-				<p>
-					Soy Sergio, estudiante del Grado de Ingenieria Informática de la Universidad Complutense de Madrid en la rama de Tecnologías de la Información.
-          Nací en Madrid en 1990, y aunque he vivido la mayor parte de mi vida en Alicante, he vuelto a esta ciudad para formarme y trabajar, pero me gustaría vivir en el extranjero para descubrir nuevas culturas.
-				</p>
-				<p>
-          Actualmente trabajo en consultoría para una multinacional, participando en varios proyectos de desarrollo de aplicaciones web a jornada casi completa.
-          La labor principal es desarrollo tanto de front-end como de back-end de aplicaciones basadas en Ionic, Node.js y AngularJS, que integran servicios REST y comunicación con dispositivos reales.
-          A la vez, dedico el resto de mi jornada laboral a mi formación académica, así como a aprender los lenguajes del mañana, como Angular 2 para poder combinar todo el conocimiento adquirido en herramientas de futuro.
-				</p>
-				<p>
-          Aun así, siempre queda tiempo libre para dedicarlo a disfrutar de mi novia, mi familia, mi gata y salir con los amigos.
-          Me gustan los juegos de ordenador, programar, el desarrollo de videojuegos, jugar al baloncesto, viajar, los idiomas, pintar miniaturas...
-				</p>
-				<p>
-          Necesito más tiempo. :)
-				</p>
-
-
-				<ul class="contactIcons">
-					<li>
-						<a href="https://github.com/RedParabola" target="_blank">
-							<img src="../images/icons/git_icon.png" alt="GitHub">
-						</a>
-					</li>
-					<li>
-						<a href="https://www.facebook.com/search/str/Sergio-Calero-Robles" target="_blank">
-							<img src="../images/icons/fb_icon.png" alt="Facebook">
-						</a>
-					</li>
-				</ul>
-			</div>
-      -->
+      <div id="info" class="descripcionPersonal">
+      </div>
 
 		</div>
 
