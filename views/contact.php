3343f24c75e76db43e676e54da344e02d547698c
Diego Valbuena Pineda <Diego Valbuena Pineda>
2017-04-30 13:36:39 +0200

    Added new php control in forms, test area of XSS injection and SQL injection

--- views/contact.php
+++ views/contact.php
@@ -13,6 +13,61 @@
 
   <body>
 
+      <?php
+      $nameErr = $emailErr = $radioErr = $conformErr = $commentErr = "";
+      $name = $email = $radio = $conform = $comment = "";
+
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+        if (empty($_POST['textNameContact'])) {
+          $nameErr="Nombre requerido";
+        } else {
+          $name = test_input($_POST["textNameContact"]);
+          if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+            $nameErr = "Solo se permiten letras y espacios";
+          }
+        }
+
+
+        if (empty($_POST['textEmailContact'])) {
+          $emailErr = "Email requerido";
+        } else {
+          $email = test_input($_POST["textEmailContact"]);
+          if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "formato de Email erroneo";
+          }
+        }
+
+
+        if (empty($_POST['radioClass'])) {
+          $radioErr = "Selecciona un tipo";
+        } else {
+          $radio = test_input($_POST['radioClass']);
+        }
+
+
+        if (empty($_POST['textEmailText'])) {
+          $commentErr = "";
+        } else {
+          $comment = test_input($_POST['textEmailText']);
+        }
+
+
+        if (empty($_POST['conform'])) {
+          $conformErr = "Acepta nuestras condiciones";
+        } else {
+          $conform = test_input($_POST['conform']);
+        }
+      }
+      function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+    ?>
+
     <!-- WRAPPER CLASS -->
     <div id="wrapper">
 
@@ -20,28 +75,24 @@
       <?php
       include '../php/navbar.php';
        ?>
-      <!-- SIDEBAR CLASS -->
-      <div class="sidebarLeft">
-      </div>
-      <!-- SIDEBAR CLASS -->
-      <div class="sidebarRight">
-      </div>
 
       <!-- CONTENT CLASS -->
       <div id="content">
-
-        <form class="contactForm" action="mailto:dvalbuen@ucm.es" method="post">
+        <p><span class="error">* We're missing some datas!!</span></p>
+        <form class="contactForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
           <div class="group">
             <fieldset>
              <legend>Personal information:</legend>
               <label id="nickLabel"></label>
-              <input type="text" name="textNameContact" id="nameContac" class="field" required autofocus>
+              <input type="text" name="textNameContact" id="nameContac" class="field" autofocus>
               <span class="highlight"></span><span class="bar"></span>
+              <span class="error">* <?php echo $nameErr;?></span>
 
               <label id="emailLabel"></label>
-              <input type="text" name="textEmailContact" id="emailContact" class="field" required>
+              <input type="text" name="textEmailContact" id="emailContact" class="field">
               <span class="highlight"></span><span class="bar"></span>
+              <span class="error">* <?php echo $emailErr;?></span>
             </fieldset>
           </div>
 
@@ -51,6 +102,7 @@
               <input type="radio" name="radioClass" class="field" value="male" checked><span id="evaluation"></span>
               <input type="radio" name="radioClass" class="field" value="male"><span id="suggest"></span>
               <input type="radio" name="radioClass" class="field" value="male"><span id="review"></span>
+              <span class="error"><?php echo $radioErr;?></span>
             </fieldset>
           </div>
 
@@ -59,13 +111,13 @@
           </div>
 
           <div class="groupConform">
-            <input type="checkbox" name="conform" value="conformity" required=""/><span id="accordance" ></span><br>
+            <input type="checkbox" name="conform" value="conformity"/><span id="accordance" ></span>
+            <span class="error">* <?php echo $conformErr;?></span><br>
           </div>
 
           <div class="group">
             <input type="submit" value="Submit">
           </div>
-
         </form>
 
       </div>
