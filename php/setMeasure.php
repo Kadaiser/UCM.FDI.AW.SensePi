c0c3023dc65755c3115131e35e3468794c5f9f7a
FASUS\Kadaiser <dvalbuen@ucm.es>
2017-05-10 19:46:44 +0200

    fisrt aproach to ajax php petition

--- php/setMeasure.php
+++ php/setMeasure.php
@@ -5,6 +5,28 @@
     <title>Injector para vagos</title>
         <link rel="stylesheet" type="text/css" href="../css/basic.css">
         <link rel="stylesheet" type="text/css" href="../css/contact.css"/>
+
+        <script>
+        function populateMeasure(id){
+          $('#mesureSelect').empty()
+          var dropDown = document.getElementById("carId");
+          var roomId = dropDown.options[dropDown.selectedIndex].value;
+          $.ajax({
+            type: "POST",
+            url: "../php/getMeasureTracks.php",
+            data: { 'roomId': roomId  },
+            success: function(data){
+                // Parse the returned json data
+                var opts = $.parseJSON(data);
+                // Use jQuery's each to iterate over the opts value
+                $.each(opts, function(i, d) {
+                    // You will need to alter the below to get the right values from your json object.
+                    $('#emptyDropdown').append('<option value="' + d.measuretrack + '">' + d.roomslotid + '</option>');
+                });
+            }
+          });
+        }
+        </script>
   </head>
 
   <body>
@@ -13,21 +35,24 @@
       $DBconnection = mysqli_connect('127.0.0.1','root','','pisense');
       $sqlroom = 'SELECT id,name FROM rooms';
       $queryRoom = mysqli_query($DBconnection,$sqlroom);
-      $sqlmeasureTrack  = 'SELECT roomslotid, measuretrack FROM measurelogs';
-      $queryMeasureTrack = mysqli_query($DBconnection,$sqlmeasureTrack);
 
       $optionRoom='';
       while ($row = $queryRoom->fetch_array()) {
         $optionRoom.='<option value="'.$row['id'].'">'.$row['name'].'</option>';
       }
 
+      /*
+      $sqlmeasureTrack  = 'SELECT roomslotid, measuretrack FROM measurelogs';
+      $queryMeasureTrack = mysqli_query($DBconnection,$sqlmeasureTrack);
+
       $option='';
       while ($row = $queryMeasureTrack->fetch_array()) {
         $option.='<option value="'.$row['roomslotid'].'">'.$row['measuretrack'].'</option>';
       }
+      */
 
-      $tempErr = $humErr = $noiseErr = "";
-      $temp = $hum = $noise = $result = "";
+      $tempErr = $humErr = $noiseErr = $dateError = "";
+      $temp = $hum = $noise = $result = $date = "";
 
       if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -46,9 +71,14 @@
         }
         $noise = test_input($_POST['noise']);
 
+        if (empty($_POST['datetime'])) {
+          $dateError = "No se ha asignado una fecha";
+        }
+        $date = test_input($_POST['datetime']);
+
         /*debug*/
         $result  = "Se ha procesado el registro con valores Temp:".$temp." Hum:".$hum." Noise:".$noise.
-                    " en la sala:".$_POST['Room']." para el track:".$_POST['MeasureTrack'];
+                    " en la sala:".$_POST['Room']." para el track:".$_POST['MeasureTrack']." con fecha ".$_POST['datetime'];
       }
 
       function test_input($data) {
@@ -61,30 +91,53 @@
 
     <form class="injector" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
-      <label for="Room">Room</label>
-      <select class="mesureTrackSelect" name="Room">
-        <?php echo $optionRoom;?>
-      </select><br>
+      <div class="group">
+        <label>Room</label>
+        <select onchange="populateMeasure(this.value)" class="mesureTrackSelect" name="Room">
+          <?php echo $optionRoom;?>
+        </select>
+      </div>
 
-      <label for="MeasureTrack">Measure Track</label>
-      <select class="mesureTrackSelect" name="MeasureTrack">
-        <?php echo $option;?>
-      </select><br>
+      <div class="group">
+        <label>Measure Track</label>
+        <select onchange="showImput()" id="mesureSelect" class="mesureTrackSelect" name="MeasureTrack">
+          <?php echo $option;?>
+        </select>
+      </div>
+      <br>
 
-      <label for="temp">Temperatura Cº </label>
-      <input type="number" name="temp" min="-50" max="80">
-      <span class="error"><?php echo $tempErr;?></span><br>
+      <span class="error"><?php echo $tempErr;?></span>
+      <div class="group">
+        <input type="number" name="temp" min="-50" max="80">
+        <span class="highlight"></span><span class="bar"></span>
+        <label>Temperatura (Cº)</label>
+      </div>
 
-      <label for="hum">Humedad % </label>
-      <input type="number" name="hum" min="0" max="100">
-      <span class="error"><?php echo $humErr;?></span><br>
+      <span class="error"><?php echo $humErr;?></span>
+      <div class="group">
+        <input type="number" name="hum" min="0" max="100">
+        <span class="highlight"></span><span class="bar"></span>
+        <label>Humedad (%)</label>
+      </div>
 
-      <label for="noise">Noise dB </label>
-      <input type="number" name="noise" min="0" max="100">
-      <span class="error"><?php echo $noiseErr;?></span><br>
+      <span class="error"><?php echo $noiseErr;?></span>
+      <div class="group">
+        <input type="number" name="noise" min="0" max="100">
+        <span class="highlight"></span><span class="bar"></span>
+        <label>Ruido (dB)</label>
+      </div>
 
-      <input type="submit" value="Process">
-      <span class="error"><?php echo $result;?></span><br>
+      <span class="error"><?php echo $dateError;?></span>
+      <div class="group">
+        <input type="datetime-local" name="datetime" min="2017-05-01" required >
+        <span class="highlight"></span><span class="bar"></span>
+        <label>Date Time: (dB)</label>
+      </div>
+
+      <div class="group">
+        <input type="submit" value="Process">
+        <span class="error"><?php echo $result;?></span>
+      </div>
 
     </form>
   </body>
