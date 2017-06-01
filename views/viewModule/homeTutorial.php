f67926da4792390a34463e4ef9724abf1e624daa
FASUS\Kadaiser <dvalbuen@ucm.es>
2017-06-01 16:26:25 +0200

    Tutorial working, still havethe FOG bug

--- views/viewModule/homeTutorial.php
+++ views/viewModule/homeTutorial.php
@@ -1,51 +1,51 @@
 <div id="firstVisit">
   <div class="tutorial" id="tutorial1">
-  <h1>Welcome To PiSense Project</h1>
-  <p>It seems it´s your first visit to our site... Nice! What you have here is a map of the fisrt floor of the FDI!! </p>
-  <h2>Let me introduce you our features:</h2>
-  <ul>
-    <li>
-    Click on the rooms to kwon the temperature and humidity, even a nice graph of the last week.
-    </li>
-    <li>
-    Whenever you open a room, if you're register as user, you can add a room to your favorite list.
-    </li>
-    <li>
-    The span appears when you click on a room, you can return to the map with the close button on the corner...
-    </li>
-    <li>
-    Or clicking on that fog on the background.
-    </li>
-    <li>
-    Feel free to explore our proyect section to know more about us.
-    </li>
-    <li>
-    Oh Yeah! the contact secction is not working yet ok? But we have the intention!
-    </li>
-  </ul>
-  <div id="next" onclick="toggleTutorial()" title="Show me more"></div>
-</div>
+    <h1>Welcome To PiSense Project</h1>
+    <p>It seems it´s your first visit to our site... Nice! What you have here is a map of the fisrt floor of the FDI!! </p>
+      <h2>Let me introduce you our features:</h2>
+    <ul>
+      <li>
+      Click on the rooms to kwon the temperature and humidity, even a nice graph of the last week.
+      </li>
+      <li>
+      Whenever you open a room, if you're register as user, you can add a room to your favorite list.
+      </li>
+      <li>
+      The span appears when you click on a room, you can return to the map with the close button on the corner...
+      </li>
+      <li>
+      Or clicking on that fog on the background.
+      </li>
+      <li>
+      Feel free to explore our proyect section to know more about us.
+      </li>
+      <li>
+      Oh Yeah! the contact secction is not working yet ok? But we have the intention!
+      </li>
+    </ul>
+    <div id="next" onclick="toggleTutorial()" title="Show me more"></div>
+  </div>
 
-<div class="tutorial" id="tutorial2">
-  <h1>Your DashBoard... Your Way!</h1>
-  <p>Whenever you log in our web, you will get redirect to your dashboard, a prety nice configurable set of 9 spaces, ready to be fullfiled with your personal needs </p>
-    <h2>How to do it??</h2>
-  <ul>
-    <li>
-    Click on a clear space, it will show you the list of cards
-    </li>
-    <li>
-    Fullfill as many spaces you want with the cards.
-    </li>
-    <li>
-    Confirm the config and save it. Settel, it will be memorize on the system the next time you come
-    </li>
-    <li>
-    We really are waiting for you to yoin us!
-    </li>
-  </ul>
-  <div id="last" onclick="toggleTutorial()" title="return"></div>
-</div>
+  <div class="tutorial" id="tutorial2">
+    <h1>Your DashBoard... Your Way!</h1>
+    <p>Whenever you log in our web, you will get redirect to your dashboard, a prety nice configurable set of 9 spaces, ready to be fullfiled with your personal needs </p>
+      <h2>How to do it??</h2>
+    <ul>
+      <li>
+      Click on a clear space, it will show you the list of cards
+      </li>
+      <li>
+      Fullfill as many spaces you want with the cards.
+      </li>
+      <li>
+      Confirm the config and save it. Settel, it will be memorize on the system the next time you come
+      </li>
+      <li>
+      We really are waiting for you to yoin us!
+      </li>
+    </ul>
+    <div id="last" onclick="toggleTutorial()" title="return"></div>
+  </div>
 
-<div class="closeSpam" onclick="tutorialBox()" title="Close tutorial"></div>
+  <div class="closeSpam" onclick="tutorialBox()" title="Close tutorial"></div>
 </div>
