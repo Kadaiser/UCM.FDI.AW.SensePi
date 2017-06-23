9148046746a3aab54d0763ef715903d090d1416f
Diego Valbuena Pineda <Diego Valbuena Pineda>
2017-06-23 09:58:57 +0200

    modified DB connection

--- php/DBconnection.php
+++ php/DBconnection.php
@@ -1,5 +1,5 @@
 <?php
-  $connection = mysqli_connect('127.0.0.1','root','','pisense')
+  $connection = mysqli_connect('127.0.0.1','pisense','Firlollito21.12','pisense')
   or die(header("Location: ../views/error.php"));
   mysqli_set_charset( $connection, 'utf8');
 ?>
